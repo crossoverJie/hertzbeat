c8bfa5daceb1dc162e65086486d3e69c859bc3e7
crossoverJie <crossoverJie@gmail.com>
2024-04-23 13:52:58 +0800

    tmp

--- collector/src/test/java/org/apache/hertzbeat/collector/collect/dns/DnsCollectImplTest.java
+++ collector/src/test/java/org/apache/hertzbeat/collector/collect/dns/DnsCollectImplTest.java
@@ -18,38 +18,102 @@
 package org.apache.hertzbeat.collector.collect.dns;
 
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.hertzbeat.common.entity.job.Metrics;
 import org.apache.hertzbeat.common.entity.job.protocol.DnsProtocol;
 import org.apache.hertzbeat.common.entity.message.CollectRep;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.xbill.DNS.Header;
+import org.xbill.DNS.Message;
+import org.xbill.DNS.Resolver;
 
 
 /**
  * Test case for {@link DnsCollectImpl}
  *
  */
+@ExtendWith(MockitoExtension.class)
 public class DnsCollectImplTest {
     private DnsProtocol dnsProtocol;
+    @InjectMocks
     private DnsCollectImpl dnsCollect;
+
+    @Mock
+    private Resolver res;
+
+    @Mock
+    private Message response;
+
+    @Mock
+    private Header header;
 
     @BeforeEach
     public void setup() {
         dnsCollect = new DnsCollectImpl();
         dnsProtocol = DnsProtocol.builder()
-                .dnsServerIP("8.8.8.8")
-                .address("www.google.com")
+                .dnsServerIP("61.128.130.232")
+                .address("www.baidu.com")
+                .tcp("true")
+                .port("53")
+                .timeout("1000")
                 .build();
     }
 
     @Test
     public void testCollect() {
         CollectRep.MetricsData.Builder builder = CollectRep.MetricsData.newBuilder();
+        List<String> aliasField = new ArrayList<>();
+        aliasField.add("section0");
         long monitorId = 666;
         String app = "testDNS";
         Metrics metrics = new Metrics();
+        metrics.setName("additional");
         metrics.setDns(dnsProtocol);
+        metrics.setAliasFields(aliasField);
 
         dnsCollect.collect(builder, monitorId, app, metrics);
+        for (CollectRep.ValueRow valueRow : builder.getValuesList()) {
+            System.out.println(valueRow.getColumns(0));
+            System.out.println(valueRow.getColumns(1));
+        }
+    }
+
+    @Test
+    public void testHeaderTest() throws IOException {
+        DnsProtocol dnsProtocol = DnsProtocol.builder()
+                .dnsServerIP("8.8.8.8")
+                .address("www.google.com")
+                .tcp("true")
+                .port("53")
+                .timeout("1000")
+                .build();
+        CollectRep.MetricsData.Builder builder = CollectRep.MetricsData.newBuilder();
+        List<String> aliasField = new ArrayList<>();
+        aliasField.add("responseTime");
+        aliasField.add("opcode");
+        long monitorId = 666;
+        String app = "testDNS";
+        Metrics metrics = new Metrics();
+        metrics.setName("header");
+        metrics.setDns(dnsProtocol);
+        metrics.setAliasFields(aliasField);
+
+        Mockito.when(res.send(Mockito.any())).thenReturn(response);
+        Mockito.when(response.getHeader()).thenReturn(header);
+        Mockito.when(header.getOpcode()).thenReturn(1);
+
+        dnsCollect.collect(builder, monitorId, app, metrics);
+        for (CollectRep.ValueRow valueRow : builder.getValuesList()) {
+            System.out.println(valueRow.getColumns(0));
+            System.out.println(valueRow.getColumns(1));
+        }
     }
 }
